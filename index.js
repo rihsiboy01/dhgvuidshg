b0b1ecf131daf13ac4fc1784122fe18c73a8f219
rihsiboy01 <auzcom78@gmail.com>
2024-11-11 16:47:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let a = document.querySelector('h1');  
+let l = document.querySelector('h2');  
+let s = document.querySelector('h3');  
+
+
+let b = prompt('Ismingizni kiriting');        
+let g = prompt('Familiyangizni kiriting');    
+let d = prompt('Yoshingizni kiriting');       
+
+a.innerHTML = b;  
+l.innerHTML = g;  
+s.innerHTML = d; 
+
